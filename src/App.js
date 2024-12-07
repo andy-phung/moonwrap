cb659a8dfc6f53b4114610c2114599090f37ec8b
Andy <andyphung64@gmail.com>
2024-12-07 05:50:08 -0500

    might be cooked

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div>
+        landing div
+      </div>
+      <div>
+        landing div
+      </div>
+      <div>
+        landing div
+      </div>
+      <div>
+        landing div
+      </div>
+    </div>
+  );
+}
+
+export default App;
